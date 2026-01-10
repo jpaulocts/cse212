571e1a9879c62da0b661f826f5703277a513c32b
jpaulocts <jpaulocts@gmail.com>
2026-01-10 20:31:56 -0300

    code assignment

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -9,11 +9,20 @@ public static class Arrays
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // 1# Creating an array to store multiples
+        double[] multiples = new double[length];
+
+        //2# Iterating through length
+
+        for (int i = 0; i < length; i++)
+        {
+            //3# adding multiples to the array
+            multiples[i] = number * (i + 1);
+        }
+
+        //4# returning the array
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -26,8 +35,17 @@ public static class Arrays
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        //1# We create two lists : one for last part and another to the first part
+        List<int> last = new List<int>(amount);
+        List<int> first = new List<int>(data.Count - amount);
+
+        //2# We slice the list based on given amount
+        last = data.GetRange(data.Count - amount, amount);
+        first = data.GetRange(0, data.Count - amount);
+        //3# We erase the original list
+        data.Clear();
+        //4# We add the list rotated
+        data.AddRange(last);
+        data.AddRange(first);
     }
 }
